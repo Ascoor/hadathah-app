7e07ea3d087407def9ccec0ef06cd74bec2c2af8
askar <mr.askar1984@gmail.com>
2024-05-03 19:34:01 +0300

    as

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -7,16 +7,22 @@ use App\Models\Designer;
 use App\Models\Offer;
 use App\Models\Product;
 use App\Models\SaleRep;
+use App\Models\SocialRep;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
     public function index()
     {
+        $designersCount = Designer::count();
+        $saleRepsCount = SaleRep::count();
+        $socialRepsCount = SocialRep::count();
+        $employeesCount = $designersCount + $saleRepsCount + $socialRepsCount;
         return response()->json([
             'customers' => Customer::count(),
             'products' => Product::count(),
-            'designers' => Designer::count(),
+            'employees' => $employeesCount,
+
             'sales' => SaleRep::count(),
             'offers' => Offer::count(),
         ]);
