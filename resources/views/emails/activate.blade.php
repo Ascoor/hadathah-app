a3355a85c104b962b79c830de3710b970a9c4dd8
askar <mr.askar1984@gmail.com>
2024-06-08 10:17:04 +0300

    sd

--- resources/views/emails/activate.blade.php
+++ resources/views/emails/activate.blade.php
@@ -50,15 +50,15 @@
     <!-- Main content area with higher z-index -->
     <div class='z-50 flex flex-col items-center justify-center mt-10 w-full px-4'>
       <div class='flex justify-center items-center text-white space-x-4 mb-10'>
-        <h1 class="text-4xl md:text-6xl lg:text-8xl font-bold" style="text-shadow: 2px 2px 16px rgba(0,0,0,0.5);">
+        <h1 class="text-4xl md:text-6xl lg:text-8xl text-white font-bold" style="text-shadow: 2px 2px 16px rgba(0,0,0,0.5);">
           Hadathah<sup class="text-2xl md:text-4xl">&reg;</sup>
         </h1>
       </div>
       <div class='relative mx-auto p-4 w-full md:max-w-2xl lg:max-w-3xl bg-white bg-opacity-90 rounded-xl shadow-xl'>
         <div class='flex flex-col md:flex-row items-center justify-around'>
           <div class='md:w-1/2 p-4 rounded-lg shadow-inner text-center'>
-          <h1 class="text-2xl md:text-2xl lg:text-4xl mb-4 font-bold">مرحبًا بك، {{ $user->name }}</h1>
-                    <p class="text-xl mb-4">يمكنك تفعيل حسابك بالضغط على زر تفعيل الحساب</p>  
+          <h1 class="text-2xl md:text-2xl lg:text-4xl text-white mb-4 font-bold">مرحبًا بك، {{ $user->name }}</h1>
+                    <p class="text-xl mb-4 text-white">يمكنك تفعيل حسابك بالضغط على زر تفعيل الحساب</p>  
 
           <a href="{{ url('https://api.hadathah.org/activate/' . $user->activation_code) }}" class="active-button inline-block">تفعيل الحساب</a>
         </div>
